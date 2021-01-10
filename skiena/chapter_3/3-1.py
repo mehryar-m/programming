331614f7de93a2fff7a053ca160eed86ee7e2e3b
mehryar-m <m.maalem@outlook.com>
2021-01-09 18:12:35 -0800

    soln: with bonus point of finding index

--- skiena/chapter_3/3-1.py
+++ skiena/chapter_3/3-1.py
@@ -2,6 +2,22 @@
 Give an algorithm that returns true if a string properly contains balanced paranthesis.
 """
 
+def is_balanced_which_index(input_string):
+    count = 0
+    for i in range(0, len(input_string)):
+        if input_string[i] == "(":
+            count += 1
+        if input_string[i] == ")":
+            count -= 1
+        if count < 0: 
+            print("index with an issue %i" %i)
+            return False
+    
+    if count != 0:
+        print("index with an issue is %i" %(len(input_string) - 1))
+        return False
+    return True
+    
 def is_balanced(input_string):
     number_of_open_paranthesis = []
 
@@ -23,4 +39,12 @@ if __name__ == "__main__":
     assert is_balanced("()()()()())") == False
     assert is_balanced("()))))") == False
     assert is_balanced("())(") == False
+
+    assert is_balanced_which_index(")(") == False
+    assert is_balanced_which_index("())(") == False
+    assert is_balanced_which_index("()()()()())") == False
+    assert is_balanced_which_index("()()()()()") == True
+
+
+
 
